b86c2874dfd35074a36db693fa77180edaefb532
Sally0601 <q4929662@gmail.com>
2024-06-30 18:19:13 +0900

    商品出品ページを表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,22 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   def index
+    @items = Item.all.order(created_at: :desc)
   end
+
+  def new
+    @item = Item.new
+  end  
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end 
 
   private
 
