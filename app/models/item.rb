dcd78cc88a35ec7b134c82d9cb0d92ae4e2df008
Sally0601 <q4929662@gmail.com>
2024-06-30 17:53:02 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,4 +11,18 @@ class Item < ApplicationRecord
 
   has_one_attached :image
 
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :description, presence: true, length: { maximum: 1000 }
+  validates :category_id, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :condition_id, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :delivery_charge_id, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :region_id, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :preparation_id, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+
+  validates :image, presence: true
+
+  def image_presence
+    errors.add(:image, "can't be blank") unless image.attached?
+  end
 end
